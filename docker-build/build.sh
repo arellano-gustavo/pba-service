be5fccb5f9ca97b06d9f700981cd85b1b5a5f4b9
Gustavo Arellano Sandoval <arellano.gustavo@gmail.com>
2019-06-13 18:28:57 -0500

    push 4

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -2,7 +2,7 @@ export NAME=nexus.ci.gustavo-arellano.com:5005/pba-service
 export FILE=pba-service-0.0.1-SNAPSHOT-fat.jar
 export SERVICE=pba-service
 export RUTA=/home/ubuntu/pba/
-export UPDATE=$(date -r $RUTA$FILE)
+export UPDATE=echo $(($(date +'%s * 1000 + %-N / 1000000')))
 
 echo $UPDATE
 
