93e0b14202c9a3e41c41e747b9fb32bca61c23b5
Gustavo Arellano Sandoval <arellano.gustavo@gmail.com>
2019-06-13 18:24:40 -0500

    push 2

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -2,10 +2,13 @@ export NAME=nexus.ci.gustavo-arellano.com:5005/pba-service
 export FILE=pba-service-0.0.1-SNAPSHOT-fat.jar
 export SERVICE=pba-service
 export RUTA=/home/ubuntu/pba/
+export UPDATE=$(date -r $RUTA$FILE)
+
+echo $UPDATE
 
 cd ..
 
 docker build $RUTA -t $NAME
 docker push $NAME
-pba/refresh.sh $SERVICE $NAME $(date -r $RUTA$FILE)
+pba/refresh.sh $SERVICE $NAME $UPDATE
 
