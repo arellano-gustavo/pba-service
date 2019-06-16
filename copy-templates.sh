3645b6fe8835c89d9b45f7a4cca3f3b1b92b211c
Gustavo Arellano Sandoval <arellano.gustavo@gmail.com>
2019-06-16 01:13:02 -0500

    adding replacer plugin
    cloning conf from crypto-back pom

---
+++ copy-templates.sh
@@ -0,0 +1,3 @@
+# curl http://jenkins.cgtic.conacyt.mx/generic-webhook-trigger/invoke?token=011
+echo "******************** COPIANDO TEMPLATES PARA REEMPLAZO *****************************"
+cp docker-build-templates/*.* docker-build
